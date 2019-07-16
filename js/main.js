01ad2fda2839fbea97635bf64a5e2c5a3dad1682
Matthew Esparza <39199348+matthewesp@users.noreply.github.com>
2019-07-16 15:38:16 -0500

    Fixed bugs with smooth scrolling going below the the anchor.

--- js/main.js
+++ js/main.js
@@ -7,15 +7,19 @@ for (let item of anchorlinks) {
     item.addEventListener('click', (e) => {
         let hashval = item.getAttribute('href')
         let target = document.querySelector(hashval)
-        target.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        })
+        const yCoordinate = target.getBoundingClientRect().top + window.pageYOffset;
+        const yOffset = -60;
+
+        window.scrollTo({
+            top: yCoordinate + yOffset,
+            behavior: 'smooth'
+        });
         history.pushState(null, null, hashval)
         e.preventDefault()
     })
 }
 
-
+//jquery spyscroll
+$('body').scrollspy({ target: '#navbar' })
 
 console.log("hello there");
